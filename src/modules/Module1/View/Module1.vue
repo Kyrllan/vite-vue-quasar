86314a063cc7951680803d6b365ae5f6af7abf38
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-09-01 07:55:14 -0300

    added esri maps on module 1

--- src/modules/Module1/View/Module1.vue
+++ src/modules/Module1/View/Module1.vue
@@ -1,12 +1,40 @@
+<script setup>
+import { loadModules } from 'esri-loader';
+
+loadModules(['esri/views/MapView', 'esri/WebMap'])
+  .then(([MapView, WebMap]) => {
+    //esriConfig.apiKey = process.env.ESRI_API_KEY;
+    const webmap = new WebMap({
+      portalItem: { // autocasts as new PortalItem()
+        id: 'bc19c34549de4000bcd91cef929420dc'
+      }
+    });
+    const view = new MapView({
+      map: webmap,
+      container: 'esriMap',
+      zoom: 5,
+      center: [-51.531047, -24.695827],
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+</script>
+
 <template>
-  <q-page-container>
-    <q-page>
-      <h1>Módulo 1</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore
-        vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi
-        perferendis voluptate?
-      </p>
-    </q-page>
-  </q-page-container>
+  <q-page>
+    <h1>Módulo 1</h1>
+    <div id="esriMap"></div>
+  </q-page>
 </template>
+
+
+<style scoped>
+@import url('https://js.arcgis.com/4.4/esri/themes/light/main.css');
+
+#esriMap {
+  width: 800px;
+  height: 500px;
+}
+</style>
--- src/modules/Module3/View/Module3.vue
+++ src/modules/Module3/View/Module3.vue
@@ -1,12 +1,10 @@
 <template>
-  <q-page-container>
-    <q-page>
-      <h1>Módulo 3</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore
-        vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi
-        perferendis voluptate?
-      </p>
-    </q-page>
-  </q-page-container>
+  <q-page>
+    <h1>Módulo 3</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore
+      vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi
+      perferendis voluptate?
+    </p>
+  </q-page>
 </template>
