73521543fc885e3a3b62370b147ca3a0f9067661
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-09-08 13:45:08 -0300

    arcgis added

--- src/modules/Module1/View/Module1.vue
+++ src/modules/Module1/View/Module1.vue
@@ -32,13 +32,29 @@ let view = null;
 let sketchViewModel = null;
 let sketchLayer = null;
 let sketchWidget = null;
-
-let number = 12
+let graphicsLayer = null;
 
 /* let popupTemplate = new PopupTemplate({
-  title: "Population by Gender",
-  content: setContentInfo
-}); */
+  title: "Atributos",
+  content: setGraphicAttributes
+});
+
+function setGraphicAttributes(feauture) {
+  return `
+        <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" value="${feauture.attributes.attr1}" disabled>
+
+            <label for="population">Population:</label>
+            <input type="number" id="population" name="population" value="${feauture.attributes.attr2}">
+
+            <label for="area">Area (sq. miles):</label>
+            <input type="number" id="area" name="area" value="${feauture.attributes.attr3}">
+
+            <button type="submit">Save</button>
+        </form>
+    `;
+} */
 
 function createMap() {
   map = new Map({
@@ -58,7 +74,9 @@ function createMap() {
     layer: sketchLayer
   })
   sketchLayer = new GraphicsLayer();
+  graphicsLayer = new GraphicsLayer();
   map.add(sketchLayer);
+  map.add(graphicsLayer);
   sketchWidget = new Sketch({
     view: view,
     layer: sketchLayer,
@@ -66,41 +84,42 @@ function createMap() {
   })
   view.ui.add(sketchWidget, "top-right");
   sketchWidget.on("create", sketchCreate)
-  view.on("click", onClickView)
-  view.on("pointer-move", onMouseMoveView)
+  //view.on("click", onClickView)
+  //view.on("pointer-move", onMouseMoveView)
 }
 
 
 function sketchCreate(event) {
   if (event.state === "complete") {
-
-
-
-    event.graphic.attributes = {
-      name: `NUMERO ${number}`,
-      isSketched: true
-    }
-    //event.graphic.popupTemplate = popupTemplate
-    number++
+    sendGraphicToLayer(event.graphic)
   }
 }
 
-function onClickView(event) {
-  console.log(event)
+function sendGraphicToLayer(graphic) {
+  graphic.attributes = {
+    attr1: "ATRIBUTO 1",
+    attr2: "ATRIBUTO 2",
+    attr3: "ATRIBUTO 3",
+  }
+  console.log('GRAPHIC', graphic)
+  //graphic.popupTemplate = popupTemplate
+  //graphicsLayer.add(newGraphic);
 }
 
-function onMouseMoveView(event) {
+/* function onClickView(event) {
+  console.log(event)
+}*/
+
+/* function onMouseMoveView(event) {
   view.hitTest(event).then(function (response) {
     if (response.results.length > 0) {
       let graphic = response.results[0].graphic;
       if (graphic.attributes.isSketched) {
-        console.log('GRA',graphic)
+        console.log('GRA', graphic)
         view.openPopup({
           location: graphic.geometry.centroid,
-          content: `<div>
-            <div>${graphic.attributes.name}</div>
-          </div>`
-        });
+          content: `<div>${graphic.attributes.name}</div>`
+        })
       } else {
         view.closePopup();
       }
@@ -108,7 +127,7 @@ function onMouseMoveView(event) {
       view.closePopup();
     }
   });
-}
+}  */
 
 onMounted(async () => {
   await createMap()
@@ -128,8 +147,6 @@ onUnmounted(() => {
 
 
 <style scoped>
-@import url('https://js.arcgis.com/4.4/esri/themes/light/main.css');
-
 #esriMap {
   width: 100%;
   height: calc(100vh - 50px);
